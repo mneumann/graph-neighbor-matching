a766fae087dd4edabfde8348884b8571a5bfff4a
Michael Neumann <mneumann@ntecs.de>
2019-04-10 15:54:26 +0200

    Refactor benchmarks
    * Do not include load_graph() in the benchmark
    * Limit time of some benchmarks

--- benches/score_graphs.rs
+++ benches/score_graphs.rs
@@ -2,65 +2,101 @@
 mod common;
 
 use common::{load_graph, score_graphs};
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Benchmark, Criterion};
+use graph_neighbor_matching::graph::OwnedGraph;
 
-fn bench_score_graphs(c: &mut Criterion) {
-    c.bench_function("score_graphs(collect_distribute_3_3, a, 79)", |b| {
-        let g = load_graph("graphs/collect_distribute_3_3.gml");
-        let ga = load_graph("graphs/collect_distribute_3_3a.gml");
+const ITERS: usize = 100;
+const EPS: f32 = 0.01;
 
+fn assert_similarity(
+    expected_similarity_in_percent: usize,
+    a: &OwnedGraph<f32>,
+    b: &OwnedGraph<f32>,
+) {
+    let graph_score = score_graphs(a, b, ITERS, EPS, false);
+    assert_eq!(
+        expected_similarity_in_percent,
+        (graph_score * 100.0) as usize
+    );
+}
+
+fn bench_collect_distribute_a(c: &mut Criterion) {
+    let g_collect_distribute_3_3 = load_graph("graphs/collect_distribute_3_3.gml");
+    let g_collect_distribute_3_3a = load_graph("graphs/collect_distribute_3_3a.gml");
+
+    c.bench_function("score_graphs/collect_distribute_3_3a/79", move |b| {
         b.iter(|| {
             // Removing one link -> 79% similarity
-            assert_eq!(
-                79,
-                (score_graphs(&g, &ga, 100, 0.01, false) * 100.0) as usize
-            );
+            assert_similarity(79, &g_collect_distribute_3_3, &g_collect_distribute_3_3a);
         })
-    });
-
-    c.bench_function("score_graphs(collect_distribute_3_3, b, 64)", |b| {
-        let g = load_graph("graphs/collect_distribute_3_3.gml");
-        let gb = load_graph("graphs/collect_distribute_3_3b.gml");
-
-        b.iter(|| {
-            // Removing two links -> 64% similarity
-            assert_eq!(
-                64,
-                (score_graphs(&g, &gb, 100, 0.01, false) * 100.0) as usize
-            );
-        })
-    });
-
-    c.bench_function("score_graphs(skorpion, 44)", |b| {
-        let g = load_graph("graphs/skorpion.gml");
-        let ga = load_graph("graphs/skorpion_approx44.gml");
-
-        b.iter(|| {
-            assert_eq!(
-                44,
-                (score_graphs(&g, &ga, 100, 0.01, false) * 100.0) as usize
-            );
-        })
-    });
-
-    c.bench_function("score_graphs(isomorphic_ffl_35)", |b| {
-        let ga = load_graph("graphs/ffl/ffl_iso_1_35.gml");
-        let gb = load_graph("graphs/ffl/ffl_iso_2_35.gml");
-
-        b.iter(|| {
-            assert_eq!(1.0, score_graphs(&ga, &gb, 100, 0.01, true));
-        })
-    });
-
-    c.bench_function("score_graphs(isomorphic_ffl_100)", |b| {
-        let ga = load_graph("graphs/ffl/ffl_iso_1_100.gml");
-        let gb = load_graph("graphs/ffl/ffl_iso_2_100.gml");
-
-        b.iter(|| {
-            assert_eq!(1.0, score_graphs(&ga, &gb, 100, 0.01, true));
-        });
     });
 }
 
-criterion_group!(benches, bench_score_graphs);
+fn bench_collect_distribute_b(c: &mut Criterion) {
+    let g_collect_distribute_3_3 = load_graph("graphs/collect_distribute_3_3.gml");
+    let g_collect_distribute_3_3b = load_graph("graphs/collect_distribute_3_3b.gml");
+
+    c.bench_function("score_graphs/collect_distribute_3_3b/64", move |b| {
+        b.iter(|| {
+            // Removing two links -> 64% similarity
+            assert_similarity(64, &g_collect_distribute_3_3, &g_collect_distribute_3_3b);
+        })
+    });
+}
+
+fn bench_skorpion(c: &mut Criterion) {
+    let g_skorpion = load_graph("graphs/skorpion.gml");
+    let g_skorpion_approx44 = load_graph("graphs/skorpion_approx44.gml");
+
+    c.bench_function("score_graphs/skorpion/44", move |b| {
+        b.iter(|| {
+            assert_similarity(44, &g_skorpion, &g_skorpion_approx44);
+        })
+    });
+}
+
+fn bench_isomorphic_ffl35(c: &mut Criterion) {
+    let g_ffl_iso_1_35 = load_graph("graphs/ffl/ffl_iso_1_35.gml");
+    let g_ffl_iso_2_35 = load_graph("graphs/ffl/ffl_iso_2_35.gml");
+
+    c.bench(
+        "score_graphs",
+        Benchmark::new("isomorphic_ffl_35/100", move |b| {
+            b.iter(|| {
+                assert_eq!(
+                    1.0,
+                    score_graphs(&g_ffl_iso_1_35, &g_ffl_iso_2_35, ITERS, EPS, true)
+                );
+            });
+        })
+        .sample_size(10),
+    );
+}
+
+fn bench_isomorphic_ffl100(c: &mut Criterion) {
+    let g_ffl_iso_1_100 = load_graph("graphs/ffl/ffl_iso_1_100.gml");
+    let g_ffl_iso_2_100 = load_graph("graphs/ffl/ffl_iso_2_100.gml");
+
+    c.bench(
+        "score_graphs",
+        Benchmark::new("isomorphic_ffl_100/100", move |b| {
+            b.iter(|| {
+                assert_eq!(
+                    1.0,
+                    score_graphs(&g_ffl_iso_1_100, &g_ffl_iso_2_100, ITERS, EPS, true)
+                );
+            });
+        })
+        .sample_size(10),
+    );
+}
+
+criterion_group!(
+    benches,
+    bench_collect_distribute_a,
+    bench_collect_distribute_b,
+    bench_skorpion,
+    bench_isomorphic_ffl35,
+    bench_isomorphic_ffl100
+);
 criterion_main!(benches);
